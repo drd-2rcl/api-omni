f8335119719fb5e777933253b4160a919ec14707
Diogo <drd.rclrd@gmail.com>
2020-01-15 10:01:48 -0300

    Add connection to mongoose

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require('dotenv/config');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get('/', (req, res) => {
   return res.json({ message: 'Hi' })
