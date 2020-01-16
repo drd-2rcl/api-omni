5a17b30e0065b0fc10b17016ffb660ffc4b03884
Diogo <drd.rclrd@gmail.com>
2020-01-16 09:58:05 -0300

    Get github user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const { Router } = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hi' })
+routes.post('/devs', async (request, response) => {
+  // const { github_username } = request.body;
+
+  // const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+
+  // console.log(request.body)
+
+  return response.json({ ok: true })
 });
 
 module.exports = routes;
